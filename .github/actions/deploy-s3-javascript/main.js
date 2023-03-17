94e64608bbd4a3247f0ec096683922f86ed68cb5
MrDanGar <bea.rees14@gmail.com>
2023-03-17 13:22:49 +0100

    add action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,4 +7,19 @@ function run () {
     core.notice('Hello from my custom JS-Action');
 }
 
+function upload() {
+    // 1) get input 
+    const bucket = core.getInput('bucket', { rquired: true });
+    const bucketRegion = core.getInput('bucket-region', { require: true });
+    const distFolder = core.getInput('dis-folder', {require: true});
+
+    // 2) upload files
+    const s3URI = `s3://${bucket}`;
+    core.notice(`bucket: ${bucket}`);
+    core.notice(`bucket region: ${bucketRegion}`);
+    core.notice(`dist folder: ${distFolder}`);
+    //exec.exec(`aws s3 sync ${bucket} ${s3URI} --region ${bucketRegion}`);
+
+}
+
 run();
